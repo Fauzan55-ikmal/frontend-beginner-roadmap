c2c012bc67cec347a4bb81168aca544b320ea0bb
Fauzan Fauzia Fauzi <fauzanfauziafauzi@gmail.com>
2026-09-22 06:48:26 +0700

    feat(toast-notification): implement showToast function to create dynamic elements

--- 03-fase-3-vanilla-js-dom/14-toast-notification/script.js
+++ 03-fase-3-vanilla-js-dom/14-toast-notification/script.js
@@ -1,3 +1,21 @@
 // 1. SELEKSI ELEMEN DOM
 const toastContainer = document.getElementById("toast-container");
 const buttons = document.querySelectorAll(".buttons-grid .btn");
+
+// 2. FUNGSI UNTUK MEMBUAT & MENAMPILKAN TOAST
+function showToast(type, message) {
+  // A. Buat elemen div baru untuk toast
+  const toast = document.createElement("div");
+  toast.classList.add("toast", `toast-${type}`);
+
+  // B. Isi struktur HTML di dalam toast
+  toast.innerHTML = `
+        <div class="toast-content">
+            <span class="toast-message">${message}</span>
+        </div>
+        <button class="toast-close">&times;</button>
+    `;
+
+  // C. Tempelkan toast ke dalam container
+  toastContainer.appendChild(toast);
+}
